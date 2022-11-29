0cc112d3097f5663c5b9ce77105bc4d9da4d53ef
VirAyma21 <virginiaymararomero@gmail.com>
2022-11-29 14:15:15 -0300

    incorporar firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+
+import 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByvz0bRGVQkU4YCCjQpZ_J3Y2_VWywH3U",
+  authDomain: "bd-infopelis.firebaseapp.com",
+  projectId: "bd-infopelis",
+  storageBucket: "bd-infopelis.appspot.com",
+  messagingSenderId: "647398341537",
+  appId: "1:647398341537:web:3bd4243b11181141ff29e0",
+  measurementId: "G-2VJSKLMYFF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+ export const db = app.firestore();
