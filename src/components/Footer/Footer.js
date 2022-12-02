59ac67d654f8a8bb39ee8f3497cdbff72ef74e0a
likejosiane <josi13.b@gmail.com>
2022-12-01 22:13:26 -0500

    creación de footer y estilos

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import './Footer.css';
+import logo from '../../assets/logo200.png'; 
+import { Link } from 'react-router-dom'; 
+
+const Footer =()=>{
+  return(
+    <div className='footer-contenedor'>
+      <img className='logo-emovies' src={logo} alt="logo-emovies" /> 
+      <div className="footer-menu-contenedor"> 
+        <section className="footer-col">
+          <Link to="/" className='link-footer'>Home</Link>
+          <Link to="/" className='link-footer'>Contáctanos</Link>
+          <Link to="/" className='link-footer'>Terminos de uso</Link>
+        </section>
+        <section className="footer-col">
+        <Link to="/" className='link-footer'>Acerca de</Link>
+          <Link to="/" className='link-footer'>FAQ</Link>
+          <Link to="/" className='link-footer'>Privacidad</Link>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
