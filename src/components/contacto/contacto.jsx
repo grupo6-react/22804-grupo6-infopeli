9443b5d3dcc2460d06cd692b6efe459b7a9f0809
david <molina.angeldavid@gmail.com>
2022-12-20 21:39:32 -0300

    agregando estilos a contacto, modificando la importancia de input y corrigiendo jsx

--- src/components/contacto/contacto.jsx
+++ src/components/contacto/contacto.jsx
@@ -1,50 +1,56 @@
-import Footer from "../Footer/Footer";
-import Navbar from "../navbar/Navbar"
 import './contacto.css'
-import {useForm} from 'react-hook-form'
-import {useState} from 'react'
 
 
 export const Contacto = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = (event,data) => {
-        event.preventDefault()
-        console.log(data);
-    }
+
+    const submitContacto = e => e.preventDefault() 
 
     return (
         <div>
-                <form className='form-contacto' onSubmit={handleSubmit(onSubmit)}>
+            <form className='form-contacto' onSubmit={submitContacto}>
+                <div className='container-name-email'>
                     <label 
                         htmlFor="name">
                             Nombre:
-                            <input 
-                                id="name" 
-                                placeholder="Tu nombre"
-                                {...register('name', { required: true, maxLength: 30 })} 
-                            />
                     </label>
-                    {errors.name && errors.name.type === "required" && <span className='danger'>Requisito completar</span>}
+                    <input 
+                        id="name" 
+                        placeholder="Tu nombre"
+                        name='nombre'
+                        required
+                        onInvalid={F => F.target.setCustomValidity('Ingrese su nombre')}
+                        onInput={F => F.target.setCustomValidity('')} 
+                    />
                     <label 
                         htmlFor="email">
                             Email:
-                            <input
-                                id="email"
-                                placeholder="Tu correo electronico"
-                                {...register('email',{ required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/})}
-                            />
                     </label>
-                    {errors.email && errors.email.type === 'required' && <span className="danger">Requisito completar</span>}
-                    {errors.email && errors.email.type == 'pattern' && <span className='danger'>Ingrese un correo electronico valido</span>}
+                    <input
+                        id="email"
+                        placeholder="Tu correo electronico"
+                        name='email'
+                        required
+                        pattern='^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
+                        onInvalid={F => F.target.setCustomValidity('Ingrese un correo electronico valido')}
+                        onInput={F => F.target.setCustomValidity('')} 
+                    />
+                </div>
+                    <label
+                        htmlFor="comentario">
+                            Tu comentario:
+                    </label>
                     <textarea
                         id='comentario'
                         placeholder="Tu comentario aqui..."
-                        {...register('comentario',{required: true})}>
+                        name='comentario'
+                        required
+                        onInvalid={F => F.target.setCustomValidity('Debe rellenar este espacio')}
+                        onInput={F => F.target.setCustomValidity('')} 
+                        >
                     </textarea>
-                    {errors.comentario && errors.comentario.type === 'required' && <span className="danger">Requisito ingresar comentario</span>}
-                    <input type="submit" className='btn btn-lg btn-primary' value={'Enviar'}/>
-                </form>
+                    <button type="submit" className='btn btn-primary'>Enviar</button>
+            </form>
         </div>
     )
 
