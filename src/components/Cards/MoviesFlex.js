3d7ec05e0924a69b16055475bf521df3114efb43
Glenda76 <grpdunne@gmail.com>
2022-12-28 13:50:29 -0300

    cambios css

--- src/components/Cards/MoviesFlex.js
+++ src/components/Cards/MoviesFlex.js
@@ -20,61 +20,57 @@ export function MoviesFlex() {
     <div className={styles.moviesFlex}>
       <h3>Las mas recomendadas </h3>
         <div className="container">
-            <div className="swiperContainer">
+          <div className="swiperContainer">
            <Swiper
                modules={[Pagination, Autoplay]}
                loop={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false
-        }}
-        pagination={{
-          el: ".pagination",
-          clickable: true,
-        }}
-        slidesPerView={4}
-        breakpoints={{
-          "@0.00": {
-            slidesPerView: 1,
-            spaceBetween: 25,
-          },
-          "@0.50": {
-            slidesPerView: 1.25,
-            spaceBetween: 25,
-          },
-          "@1.00": {
-            slidesPerView: 2,
-            spaceBetween: 25,
-          },
-          "@1.25": {
-            slidesPerView: 2.5,
-            spaceBetween: 20,
-          },
-          "@1.50": {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
-          "@1.75": {
-            slidesPerView: 4.5,
-            spaceBetween: 0,
-          },
-        }}
-      >
-              
-              <ul>
-              
-                  {movies.map(( movie) => (
-                <SwiperSlide>
-                <>
-                   <MovieCard  key={movie.id} movie={movie} />
-                   
-                   </></SwiperSlide>
-                 ))}
+               autoplay={{
+               delay: 2000,
+               disableOnInteraction: false
+               }}
+               pagination={{
+               el: ".pagination",
+               clickable: true,
+               }}
+               slidesPerView={4}
+                breakpoints={{
+              "@0.00": {
+               slidesPerView: 1,
+               spaceBetween: 25,
+               },
+              "@0.50": {
+               slidesPerView: 1.25,
+               spaceBetween: 25,
+               },
+              "@1.00": {
+               slidesPerView: 2,
+               spaceBetween: 25,
+               },
+              "@1.25": {
+               slidesPerView: 2.5,
+               spaceBetween: 20,
+              },
+             "@1.50": {
+               slidesPerView: 3,
+               spaceBetween: 30,
+              },
+             "@1.75": {
+              slidesPerView: 4.5,
+              spaceBetween: 0,
+             },
+             }}
+            > 
+                <ul>
+                   {movies.map(( movie) => (
+                     <SwiperSlide>
+                       <><MovieCard  key={movie.id} movie={movie} /></>
+                     </SwiperSlide>
+                   ))}
                  </ul>
-                 </Swiper>
-        </div>
-        <div className="pagination" />
-      </div>
-      </div>
+            </Swiper>
+          </div>
+       <div className="pagination" />
+     </div>
+   </div>
   );
 }
