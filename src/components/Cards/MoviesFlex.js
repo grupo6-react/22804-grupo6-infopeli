ffd56aae6963d96c28348f044deb69cdfca0ee71
Glenda76 <grpdunne@gmail.com>
2022-12-28 12:26:29 -0300

    agregando slide a las cards

--- src/components/Cards/MoviesFlex.js
+++ src/components/Cards/MoviesFlex.js
@@ -2,9 +2,13 @@ import { useEffect, useState } from "react";
 import { get } from "../utils/Cliente";
 import { MovieCard } from "./MovieCard";
 import styles from "./moviesFlex.module.css";
-
+import { SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper";
+import { Swiper } from "swiper/react";
+import "swiper/css";
 // trayendo la lista de peliculas 
 export function MoviesFlex() {
+ 
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -12,11 +16,65 @@ export function MoviesFlex() {
       setMovies(data.results);
     });
   }, []);
-  return (
-    <ul className={styles.moviesFlex}>
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </ul>
+  return ( 
+    <div className={styles.moviesFlex}>
+      <h3>Las mas recomendadas </h3>
+        <div className="container">
+            <div className="swiperContainer">
+           <Swiper
+               modules={[Pagination, Autoplay]}
+               loop={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false
+        }}
+        pagination={{
+          el: ".pagination",
+          clickable: true,
+        }}
+        slidesPerView={4}
+        breakpoints={{
+          "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 25,
+          },
+          "@0.50": {
+            slidesPerView: 1.25,
+            spaceBetween: 25,
+          },
+          "@1.00": {
+            slidesPerView: 2,
+            spaceBetween: 25,
+          },
+          "@1.25": {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+          },
+          "@1.50": {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          "@1.75": {
+            slidesPerView: 4.5,
+            spaceBetween: 0,
+          },
+        }}
+      >
+              
+              <ul>
+              
+                  {movies.map(( movie) => (
+                <SwiperSlide>
+                <>
+                   <MovieCard  key={movie.id} movie={movie} />
+                   
+                   </></SwiperSlide>
+                 ))}
+                 </ul>
+                 </Swiper>
+        </div>
+        <div className="pagination" />
+      </div>
+      </div>
   );
 }
